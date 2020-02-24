2223b64081c82d8bd305adf4172682a3e7133fd5
Sara Halilovic <shalilovic3@etf.unsa.ba>
2020-02-24 21:19:35 +0100

    tut 15

--- monstercat/src/App.js
+++ monstercat/src/App.js
@@ -10,13 +10,17 @@ class App extends Component {
    this.state ={
     monsters:[],
     searchField: ''
-  }
+  };
 }
 
 componentDidMount(){
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then( users => this.setState({monsters : users}))
+}
+
+handleChange = (e) => {
+  this.setState({ searchField: e.target.value})
 }
   render(){
     const { monsters,searchField } = this.state;
@@ -25,9 +29,10 @@ componentDidMount(){
       )
   return (
     <div className="App">
+      <h1> Monsters Rolodex </h1>
       <SearchBox
       placeholder= "search monsters"
-      handleChange = { e => this.setState({ searchField: e.target.value})}
+      handleChange = {this.handleChange}
       />
       <CardList monsters={filteredMonsters} />
     </div>
